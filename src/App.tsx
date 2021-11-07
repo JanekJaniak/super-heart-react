c3d9fe5afb105a4b08ad0b9fc8d197fe78374f43
JanekJ <69921860+JanekJaniak@users.noreply.github.com>
2021-11-07 20:26:14 +0100

    Feat/header-component (#1)
    * Add folders and header component WIP
    
    * Add header components, folders, styles.sass and variables.sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import './App.sass';
+import './styles/styles.sass'
+
+import Header from './components/layout/header';
 
 function App() {
   return (
     <div className="App">
-    
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react"
+
+import logo from "../../img/logo.png"
+
+const Header = (props:any) => {
+  console.log( props)
+  return <Fragment>
+    <header>
+      <img 
+        className="main-logo" 
+        src={logo}
+        height="60px" 
+        width="224px"
+        alt="Heart logo"
+      ></img>
+    </header>
+  </Fragment>
+}
+
+export default Header

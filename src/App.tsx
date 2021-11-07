4acec6d4d4b026757aec1abae70f07503cb3d1e4
Janek_Janiak <janek.janiak@gmail.com>
2021-11-07 20:16:18 +0100

    Add folders and header component WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.sass';
 
+import Header from './components/layout/header';
+
 function App() {
   return (
     <div className="App">
-    
+      <Header></Header>
     </div>
   );
 }
